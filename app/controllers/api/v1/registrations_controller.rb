e15e9795b96f08683d2d8de9636924a22a3b999f
Spardutti <luisdamian.sp@gmail.com>
2024-08-11 17:18:31 -0300

    fix ( user ) - acc creationg serializer

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,13 +1,12 @@
 class Api::V1::RegistrationsController < ApplicationController
   skip_before_action :authenticate_request, only: :create
-  include ResponseHandler
   
   def create
     user = User.new(user_params)
     if user.save
-      render_success(serialize(user), :created)
+      render json: { status: 'success', user: user }, status: :created
     else
-      render_error(user.errors.full_messages.join(', '), :unprocessable_entity)
+      render json: { status: 'error', errors: user.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
