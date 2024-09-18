235c412d8a2e32d64602623ffb9bcaf5a69a6e7a
Spardutti <luisdamian.sp@gmail.com>
2024-09-18 15:44:54 -0300

    docker compose

--- churugastos/settings.py
+++ churugastos/settings.py
@@ -101,8 +101,12 @@ WSGI_APPLICATION = 'churugastos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'mydatabase'),
+        'USER': os.getenv('DB_USER', 'myuser'),
+        'PASSWORD': os.getenv('DB_PASSWORD', 'mypassword'),
+        'HOST': os.getenv('DB_HOST', 'db'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
