81ca80f2aa5cdd832c26cc44a2346c6350265ff8
Spardutti <luisdamian.sp@gmail.com>
2024-09-19 12:45:17 -0300

    db host and port

--- churugastos/settings.py
+++ churugastos/settings.py
@@ -51,9 +51,7 @@ ROOT_URLCONF = 'churugastos.urls'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "https://churu-gastos-api.onrender.com",
-    "churu-gastos-api.onrender.com",
-    "https://churu-gastos.onrender.com/",
-    "churu-gastos.onrender.com/"
+    "https://churu-gastos.onrender.com"
 ]
 
 CORS_ALLOW_CREDENTIALS = False
